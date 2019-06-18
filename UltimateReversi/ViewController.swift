48f2afabc6175fba7a69fe1d4bc7c9ad9e8afdf6
Bartosz Bilski <bilskib@gmail.com>
2019-06-18 20:48:39 +0200

    Initial Commit

---
+++ UltimateReversi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UltimateReversi
+//
+//  Created by Bartosz on 18/06/2019.
+//  Copyright © 2019 Bartosz Bilski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
